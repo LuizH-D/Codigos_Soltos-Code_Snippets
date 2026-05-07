7b5e88ca12fe54d577c51c48c254039ebbaf7723
Luiz Henrique Dutra <luizh.dutra001@outlook.com>
2026-05-06 21:06:32 -0300

    Bank commit

---
+++ Bank/Bank/Account.cs
@@ -0,0 +1,39 @@
+﻿using System.Globalization;
+
+namespace Bank {
+    internal class Account {
+        private int _nextNumber = 1;
+        private string AccountNumber;
+        private string AccountHolder;
+        private decimal Balance;
+
+    
+        //Constructors
+        public Account(string name) {
+            AccountHolder = name;
+            AccountNumber = _nextNumber++.ToString("D6");
+        }
+        public Account(string name, decimal balance) : this(name) {
+            Balance = balance;
+        }
+
+
+        //Methods      
+        public void Deposit(decimal amount) {
+            Balance += amount;
+        }
+        public void Withdraw(decimal amount) {
+            Balance -= amount + 5.00M;
+        }
+
+        public override string ToString() {
+            return "Account details: \n"
+                + "Account "
+                + AccountNumber
+                + ", Account Holder: "
+                + AccountHolder
+                + ", Balance: $"
+                + Balance.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Bank/Bank/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Globalization;
+
+namespace Bank {
+    class Program {
+        static void Main(string[] args) {
+            string name; string initialDeposit; decimal amount = 0; Account account;
+
+            //Account Holder Name
+            do {
+                Console.Write("Enter the account holder: ");
+                name = Console.ReadLine();
+                if(string.IsNullOrEmpty(name) || name.Any(char.IsDigit)) {
+                    Console.WriteLine("The name cannot be empty or contain digits");
+                }
+            } while(string.IsNullOrEmpty(name) || name.Any(char.IsDigit));
+
+            // Initial Deposit
+
+            do {
+                Console.Write("Will there be an initial deposit (y/n): ");
+                initialDeposit = Console.ReadLine().Replace(" ", "").ToLower();
+
+                if(string.IsNullOrEmpty(initialDeposit) || initialDeposit != "y" && initialDeposit != "n") {
+                    Console.WriteLine("Invalid option! Type 'y' or 'n'");
+                }
+            } while(string.IsNullOrEmpty(initialDeposit) || initialDeposit != "y" && initialDeposit != "n");
+
+            if(initialDeposit == "y") {
+                do {
+                    Console.Write("Enter the initial deposit amount: ");
+                    amount = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    if(amount <= 0.00M) {
+                        Console.WriteLine("Enter an amount greater than zero");
+                    }
+                } while(amount <= 0.00M);
+                account = new Account(name,amount);
+                Console.WriteLine($"Account data:\n{account}");
+            }
+            else {
+                account = new Account(name);
+                Console.WriteLine($"Account data:\n{account}");
+            }
+
+            //Deposit
+            do {
+                Console.Write("Enter a deposit amount: ");
+                amount = decimal.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+                if(amount <= 0.00M) {
+                    Console.WriteLine("Enter an amount greater than zero");
+                }
+            } while(amount <= 0.00M);
+            account.Deposit(amount);
+            Console.WriteLine($"Updated account data:\n{account}");
+
+            //Withdraw
+            do {
+                Console.Write("Enter a withdrawal amount: ");
+                amount = decimal.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+                if(amount <= 0.00M) {
+                    Console.WriteLine("Enter an amount greater than zero");
+                }
+            } while(amount <= 0.00M);
+            account.Withdraw(amount);
+            Console.WriteLine($"Updated account data:\n{account}");
+        }
+    }
+}
